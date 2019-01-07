2a4cb0d422fa753c5dc5ef563cf964c2c63dea65
Sainath <banelasainath@gmail.com>
2019-01-08 00:20:49 +0530

    bulk sms

--- src/app/_services/messaging.service.ts
+++ src/app/_services/messaging.service.ts
@@ -20,6 +20,21 @@ export class MessagingService{
           });
       }
 
+      
+    public sendBulkSms(sendto,message):Promise<any>{
+
+        let headersoptions = new HttpHeaders({
+            'Access-Control-Allow-Origin':"http://littleangelshighschool.org"
+          });
+
+        let apiurl="https://app.indiasms.com/sendsms/bulksms.php?username=immanual&password=outlook1&type=TEXT&sender=Angels&mobile="+sendto+"&message="+message;
+          return this.http.get(apiurl,{headers:headersoptions}).toPromise().then((data)=>{
+              console.log(data);
+          }).catch(err=>{
+              console.log(err);
+          });
+      }
+
       public sendPayfeesSms(sendto,message):Promise<any>{
 
         let headersoptions = new HttpHeaders({
--- src/app/sendgroupsms/sendgroupsms.component.ts
+++ src/app/sendgroupsms/sendgroupsms.component.ts
@@ -1,17 +1,21 @@
-  import { Component } from '@angular/core';
+  import { Component, OnInit } from '@angular/core';
 
 import { Hero }    from '../_models/hero';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MessagingService } from '../_services/messaging.service';
 
 @Component({
   selector: 'app-sendgroupsms',
   templateUrl: './sendgroupsms.component.html',
-  styleUrls: ['./sendgroupsms.component.css']
+  styleUrls: ['./sendgroupsms.component.css'],
+  providers: [MessagingService]
 })
-export class SendgroupsmsComponent {
+export class SendgroupsmsComponent implements OnInit {
   dropdownList = [];
   selectedItems = [];
   dropdownSettings = {};
-
+  messageForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private _messagingService:MessagingService){}
   ngOnInit() {
     this.dropdownList = [
       { item_id: 1, item_text: 'Mumbai' },
@@ -33,7 +37,16 @@ export class SendgroupsmsComponent {
       itemsShowLimit: 3,
       allowSearchFilter: true
     };
-  }
+    this.messageForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      message: ['', Validators.required]
+      
+  });
+}
+
+get f() { return this.messageForm.controls; }
+
   onItemSelect(item: any) {
     console.log(item);
   }
@@ -48,7 +61,29 @@ export class SendgroupsmsComponent {
 
   submitted = false;
 
-  onSubmit() { this.submitted = true; }
+  onSubmit() { this.submitted = true;
+  
+    console.log(this.messageForm);
+
+    // stop here if form is invalid
+    if (this.messageForm.invalid) {
+      return;
+  }
+  // this.loading = true;
+  // var message="Dear parent  Student Name "+this.attendanceForm.value.name +" this is to inform you that your son/daughter was Absent today on "+this.attendanceForm.value.date+ 
+  // "Little Angels high school velgode";
+
+  this._messagingService.sendBulkSms(this.messageForm.value.phone,this.messageForm.value.message).then(resp=>{
+    console.log(resp);
+   
+  })
+  alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.messageForm.value))
+  this.submitted = false;
+
+ 
+ this.messageForm.reset();
+
+  }
 
   newHero() {
     this.model = new Hero(42, '', '');
